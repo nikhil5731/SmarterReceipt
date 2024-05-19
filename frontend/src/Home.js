42959873e270e9cd024f48d5c5d982e1e840a35c
Aakarsh Kaushal <aakarsh99@icloud.com>
2024-05-20 01:13:19 +0530

    Initial commit

---
+++ frontend/src/Home.js
@@ -0,0 +1,18 @@
+import logo from './comp-logo.png'
+
+function Home() {
+  return (
+    <div>
+      <nav>
+        <div className="burger">
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+        </div>
+        <div className="logo"></div>
+      </nav>
+    </div>
+  );
+}
+
+export default Home;
