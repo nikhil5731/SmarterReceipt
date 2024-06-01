dba3015f3a1d7ac4e0505f9b386059912c0363ca
Aakarsh Kaushal <aakarsh99@icloud.com>
2024-06-01 16:50:30 +0530

    Please

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,7 @@ require('./config/passport'); // Initialize Passport configuration
 
 const app = express();
 
-app.use(cors({
-    origin: '*',
-    credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 // app.use(bodyParser.json());
@@ -35,15 +32,6 @@ app.use((err, req, res, next) => {
     console.error('Error:', err);
     res.status(500).send({ message: 'Something broke!' });
 });
-
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
