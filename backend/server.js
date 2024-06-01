088fd5773b34220c0a2e7a398d9b95d077006857
Aakarsh Kaushal <aakarsh99@icloud.com>
2024-06-01 16:58:58 +0530

    Please3

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ require('./config/passport'); // Initialize Passport configuration
 const app = express();
 
 app.use(cors({
-    origin: 'https://smarterreceipt.netlify.app', // Specific domain instead of '*'
+    origin: 'https://smartereceipt.netlify.app', // Specific domain instead of '*'
     credentials: true, // To accept credentials like cookies from the client
 }));
 
